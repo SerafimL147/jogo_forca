bddbdddedf457d5708c85a9a0a667376289c8734
Marcus Serafim <marcusserafimtks@gmail.com>
2022-12-28 17:53:37 -0300

    Update jogo_forca.py

--- jogo_forca.py
+++ jogo_forca.py
@@ -3,9 +3,13 @@ palavra_secreta = 'banana'.upper()
 acertos = 0
 erros = 0
 
-
+letras_acertadas = ''
+letras_erradas = ''
 
 while acertos != len(palavra_secreta) and erros != 6:
+    print('')
+
+
     letra = input('Digite a letra: ').upper()
     if letra in palavra_secreta:
         print('Você acertou a letra')
